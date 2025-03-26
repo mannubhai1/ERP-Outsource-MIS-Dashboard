0f9a7c0746ea008d99b77927f61ad0ba99324e01
Manurbhav Arya <cs21b057@iittp.ac.in>
2025-03-26 11:30:20 +0530

    changed api url for hosting

--- src/app/erp/[id]/page.tsx
+++ src/app/erp/[id]/page.tsx
@@ -20,9 +20,7 @@ interface ERP {
 async function getERP(id: string) {
   const baseUrl =
     process.env.NEXT_PUBLIC_BASE_URL ||
-    (process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}`
-      : "http://localhost:3000");
+    (process.env.VERCEL_URL && `https://${process.env.VERCEL_URL}`);
   const res = await fetch(`${baseUrl}/api/erps/${id}`, { cache: "no-store" });
   if (!res.ok) return null;
   return res.json();
